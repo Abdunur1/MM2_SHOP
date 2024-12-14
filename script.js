7e0dfc3732d1c0b8fc829f531a3f126a43d8024f
Abdunur1 <abdunur487@gmail.com>
2024-12-14 16:45:13 +0600

    Описание изменений

---
+++ script.js
@@ -0,0 +1,14 @@
+let burger_menu = document.querySelector(".line"),
+    menu = document.querySelector(".menu")
+
+    burger_menu.addEventListener("click",function () {
+console.log();
+
+const menu = document.querySelector(".menu")
+
+        if (menu.style.display === "none") {
+            menu.style.display = "block";
+        } else {
+            menu.style.display = "none"; 
+        }
+ })
